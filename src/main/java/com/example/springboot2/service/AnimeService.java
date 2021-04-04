e853d0f40975c74a5116132ec53a658e7a8a2023
Emerson Lopes <emersonsilvalopes1@gmail.com>
2021-04-04 19:59:47 -0300

    Implementado validation

--- src/main/java/com/example/springboot2/service/AnimeService.java
+++ src/main/java/com/example/springboot2/service/AnimeService.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -30,6 +32,7 @@ public class AnimeService {
         return animeRepository.findByName(name);
     }
 
+    @Transactional
     public Anime save(AnimePostRequestBody animePostRequestBody) {
         Anime anime = new Anime();
         anime.setName(animePostRequestBody.getName());
